6c064c9ba710b3c1c6bc78b6dab2067a373844c1
Paul Dicker <pitdicker@gmail.com>
2017-12-29 08:42:48 +0100

    Optimize XoroshiroMt Multiply

--- src/xoroshiro_mt.rs
+++ src/xoroshiro_mt.rs
@@ -92,7 +92,7 @@ impl Rng for XoroshiroMt64of128Rng {
     fn next_u32(&mut self) -> u32 {
         let s0 = self.s0;
         let mut s1 = self.s1;
-        let mult = s0.wrapping_mul(2685821657736338717);
+        let mult = (s0 as u32 as u64).wrapping_mul(3857418925 as u64);
 
         s1 ^= s0;
         self.s0 = s0.rotate_left(55) ^ s1 ^ (s1 << 14); // a, b
@@ -101,6 +101,7 @@ impl Rng for XoroshiroMt64of128Rng {
     }
 
     #[inline]
+    #[cfg(feature = "i128_support")]
     fn next_u64(&mut self) -> u64 {
         let s0 = self.s0;
         let mut s1 = self.s1;
@@ -111,6 +112,20 @@ impl Rng for XoroshiroMt64of128Rng {
         self.s1 = s1.rotate_left(36); // c
 
         (mult >> 32) as u64
+    }
+
+    #[inline]
+    #[cfg(not(feature = "i128_support"))]
+    fn next_u64(&mut self) -> u64 {
+        let s0 = self.s0;
+        let mut s1 = self.s1;
+        let (high, low) = s0.wmul(2685821657736338717);
+
+        s1 ^= s0;
+        self.s0 = s0.rotate_left(55) ^ s1 ^ (s1 << 14); // a, b
+        self.s1 = s1.rotate_left(36); // c
+
+        high << 32 | low >> 32
     }
 
     #[cfg(feature = "i128_support")]
@@ -124,5 +139,61 @@ impl Rng for XoroshiroMt64of128Rng {
 
     fn try_fill(&mut self, dest: &mut [u8]) -> Result<(), Error> {
         Ok(self.fill_bytes(dest))
+    }
+}
+
+
+
+trait WideningMultiply<RHS = Self> {
+    type Output;
+
+    fn wmul(self, x: RHS) -> Self::Output;
+}
+
+macro_rules! wmul_impl {
+    ($ty:ty, $wide:ident, $shift:expr) => {
+        impl WideningMultiply for $ty {
+            type Output = ($ty, $ty);
+
+            #[inline(always)]
+            fn wmul(self, x: $ty) -> Self::Output {
+                let tmp = (self as $wide) * (x as $wide);
+                ((tmp >> $shift) as $ty, tmp as $ty)
+            }
+        }
+    }
+}
+
+wmul_impl! { u32, u64, 32 }
+#[cfg(feature = "i128_support")]
+wmul_impl! { u64, u128, 64 }
+
+#[cfg(any(target_pointer_width = "32", not(feature = "i128_support")))]
+impl WideningMultiply for u64 {
+    type Output = (u64, u64);
+
+    // This code is a translation of the __mulddi3 function in LLVM's
+    // compiler-rt. It is an optimised variant of the common method
+    // `(a + b) * (c + d) = ac + ad + bc + bd`.
+    //
+    // For some reason LLVM can optimise the C version very well, but keeps
+    // shuffeling registers in this Rust translation.
+    #[inline(always)]
+    fn wmul(self, b: u64) -> Self::Output {
+        const LOWER_MASK: u64 = !0u64 >> 32;
+        let mut low = (self & LOWER_MASK).wrapping_mul(b & LOWER_MASK);
+        let mut t = low >> 32;
+        low &= LOWER_MASK;
+        t += (self >> 32).wrapping_mul(b & LOWER_MASK);
+        low += (t & LOWER_MASK) << 32;
+        let mut high = (t >> 32) as i64;
+        t = low >> 32;
+        low &= LOWER_MASK;
+        t += (b >> 32).wrapping_mul(self & LOWER_MASK);
+        low += (t & LOWER_MASK) << 32;
+        high += (t >> 32) as i64;
+        high += (self >> 32).wrapping_mul(b >> 32) as i64;
+
+        (high as u64, low)
     }
 }
